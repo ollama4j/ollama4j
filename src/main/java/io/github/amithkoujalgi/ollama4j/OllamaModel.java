46894f9ec4b48d4e14cd2ca6c7e78446c2d7164d
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-10-27 17:31:45 +0530

    init

--- src/main/java/io/github/amithkoujalgi/ollama4j/OllamaModel.java
+++ src/main/java/io/github/amithkoujalgi/ollama4j/OllamaModel.java
@@ -1,7 +1,7 @@
 package io.github.amithkoujalgi.ollama4j;
 
 public enum OllamaModel {
-    LLAMA2("llama2"), MISTRAL("mistral"), MEDLLAMA2("medllama2");
+    LLAMA2("llama2"), MISTRAL("mistral"), MEDLLAMA2("medllama2"), CODELLAMA("codellama"), VICUNA("vicuna"), ORCAMINI("orca-mini"), sqlcoder("sqlcoder"), WIZARDMATH("wizard-math");
 
     private final String model;
 
