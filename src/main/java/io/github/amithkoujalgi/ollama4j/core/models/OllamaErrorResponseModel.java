3469bf314bb440e1c4c6536536ffb5fd6132c662
Markus Klenke <markusklenke86@gmail.com>
2024-02-09 13:36:49 +0000

    Removes unused Imports

--- src/main/java/io/github/amithkoujalgi/ollama4j/core/models/OllamaErrorResponseModel.java
+++ src/main/java/io/github/amithkoujalgi/ollama4j/core/models/OllamaErrorResponseModel.java
@@ -1,8 +1,6 @@
 package io.github.amithkoujalgi.ollama4j.core.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 import lombok.Data;
 
 @Data
