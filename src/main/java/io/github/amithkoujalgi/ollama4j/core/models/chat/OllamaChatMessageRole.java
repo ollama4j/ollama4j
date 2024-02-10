4ca6eef8fd44329854af05421c63d8fe3738a322
Markus Klenke <markusklenke86@gmail.com>
2024-02-10 00:22:55 +0000

    Fixes JSON Binding for OllamaChatMessageRole

--- src/main/java/io/github/amithkoujalgi/ollama4j/core/models/chat/OllamaChatMessageRole.java
+++ src/main/java/io/github/amithkoujalgi/ollama4j/core/models/chat/OllamaChatMessageRole.java
@@ -1,14 +1,16 @@
 package io.github.amithkoujalgi.ollama4j.core.models.chat;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Defines the possible Chat Message roles.
  */
 public enum OllamaChatMessageRole {
     SYSTEM("system"),
     USER("user"),
-    ASSISTANT("assisstant");
+    ASSISTANT("assistant");
 
-    @SuppressWarnings("unused")
+    @JsonValue
     private String roleName;
 
     private OllamaChatMessageRole(String roleName){
