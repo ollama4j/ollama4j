88f6d007636450b10b284c30350472d832dfc627
Amith Koujalgi <amith.koujalgi@razorthink.com>
2024-07-13 18:24:48 +0530

    Docs fixes
    Signed-off-by: Amith Koujalgi <amith.koujalgi@razorthink.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
                         items: [
                             {
                                 label: 'Tutorial',
-                                to: '/docs/intro',
+                                to: '/intro',
                             },
                         ],
                     },
