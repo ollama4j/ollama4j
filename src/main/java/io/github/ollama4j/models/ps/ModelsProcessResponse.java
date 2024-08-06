422601c0fc89ff06efb168d0299269c7dda92f5b
Amith Koujalgi <koujalgi.amith@gmail.com>
2024-08-06 11:11:15 +0530

    Updated `ModelsProcessResponse` class to support `ps()` API
    Signed-off-by: Amith Koujalgi <koujalgi.amith@gmail.com>

--- src/main/java/io/github/ollama4j/models/ps/ModelsProcessResponse.java
+++ src/main/java/io/github/ollama4j/models/ps/ModelsProcessResponse.java
@@ -1,6 +1,7 @@
 package io.github.ollama4j.models.ps;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,28 +11,53 @@ import java.util.List;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ModelsProcessResponse {
+    @JsonProperty("models")
     private List<ModelProcess> models;
 
     @Data
     @NoArgsConstructor
     public static class ModelProcess {
+        @JsonProperty("name")
         private String name;
+
+        @JsonProperty("model")
         private String model;
+
+        @JsonProperty("size")
         private long size;
+
+        @JsonProperty("digest")
         private String digest;
+
+        @JsonProperty("details")
         private ModelDetails details;
-        private String expiresAt;
+
+        @JsonProperty("expires_at")
+        private String expiresAt; // Consider using LocalDateTime if you need to process date/time
+
+        @JsonProperty("size_vram")
         private long sizeVram;
     }
 
     @Data
     @NoArgsConstructor
     public static class ModelDetails {
+        @JsonProperty("parent_model")
         private String parentModel;
+
+        @JsonProperty("format")
         private String format;
+
+        @JsonProperty("family")
         private String family;
+
+        @JsonProperty("families")
         private List<String> families;
+
+        @JsonProperty("parameter_size")
         private String parameterSize;
+
+        @JsonProperty("quantization_level")
         private String quantizationLevel;
     }
 }
