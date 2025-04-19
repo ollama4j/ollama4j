79923451d08e45d0f9ab2acca76d3f5bd8238d58
amithkoujalgi <koujalgi.amith@gmail.com>
2025-04-19 23:24:44 +0530

    Updated docs

--- docs/src/components/Ollama4jUIImageCarousel/index.js
+++ docs/src/components/Ollama4jUIImageCarousel/index.js
@@ -22,20 +22,32 @@ const images = [
 ];
 
 class Ollama4jUIImageCarousel extends React.Component {
-    render() {
-      return (
-        <div style={{ width: '50%', margin: '0 auto' }}>
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                <h2>Explore the stuff we have built with Ollama4j</h2>
-                <h4>
-                    <a href="https://github.com/ollama4j/ollama4j-ui" target='_blank'>
-                        Ollama4j UI - Desktop UI built in Java with Swing
-                    </a>
-                </h4>
+    renderItem = (item) => {
+        return (
+            <div className="image-gallery-image" style={{ textAlign: 'center' }}>
+                <img
+                    src={item.original}
+                    alt=""
+                    style={{ maxHeight: '500px', width: 'auto', maxWidth: '100%' }}
+                />
             </div>
-          <ImageGallery items={images} />
-        </div>
-      );
+        );
+    };
+
+    render() {
+        return (
+            <div style={{ margin: '0 auto', maxWidth: '800px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    <h2>Explore the stuff we have built with Ollama4j</h2>
+                    <h4>
+                        <a href="https://github.com/ollama4j/ollama4j-ui" target='_blank' rel="noopener noreferrer">
+                            Ollama4j UI - Desktop UI built in Java with Swing
+                        </a>
+                    </h4>
+                </div>
+                <ImageGallery items={images} renderItem={this.renderItem} />
+            </div>
+        );
     }
 }
 
